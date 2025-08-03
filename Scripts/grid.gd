cb18f5e6eaa8799316d2b3110bd6bcd8392165c9
y1z <whyis2cool@gmail.com>
2025-08-03 15:06:19 -0600

    all the things

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -1,30 +1,55 @@
-extends Node
+extends Node2D
 
 const DEFAULT_ROWS:int = 32
 const DEFAULT_COLUMNS:int = 32
 
 @export_group("VARIABLES")
 
-@export var rows :int = 0 ;
+@export var rows :int = 0;
 @export var columns :int  = 0;
+@export var grid_size: Vector2 = Vector2(32,32)
+
 var grid_area: Area2D;
-var grid_shape: CollisionShape2D;
-# Called when the node enters the scene tree for the first time.
+var grid_rectangle: RectangleShape2D;
+
 func _ready() -> void:
 	grid_area = %GridArea
-	grid_shape = %Shape
+	grid_rectangle = %Shape.shape
+	grid_rectangle.size = grid_size
 	if rows < 2:
 		rows = DEFAULT_ROWS
-		
+
 	if columns < 2:
 		columns = DEFAULT_COLUMNS
-		
-	pass # Replace with function body.
+	
+	print_position_in_grid()
+	pass
 
-func get_postion_in_grid(x:int, y:int):
+func get_postion_in_grid(x:int, y:int) -> Vector2:
 	assert(x > -1 && x <= rows)
 	assert(y > -1 && y <= columns)
-	grid_shape.shape.get_rect().size
+	var result :Vector2 = Vector2();
+	var rect :Rect2 = grid_rectangle.get_rect()
+	var delta :Vector2 = rect.end - rect.position
+	var inverse_size : Vector2 = Vector2(1.0/rows, 1.0/columns);
+	
+	result.x = rect.position.x + position.x + (inverse_size.x * x * delta.x);
+	result.y = rect.position.y + position.y + (inverse_size.y * y * delta.y)
+	
+	return result
+
+func print_position_in_grid() ->void:
+	@warning_ignore_start("integer_division")
+	var half_columns :int = columns / 2;
+	@warning_ignore_restore("integer_division")
+	for i in range(rows):
+		for j in range(half_columns):
+			var grid_pos : Vector2  = self.get_postion_in_grid(i,j)
+			MyDebug.dprint(grid_pos)
 	
 	pass
+	
+	
+	
+	
 	
